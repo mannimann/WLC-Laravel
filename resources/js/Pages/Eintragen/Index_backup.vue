d8b2fd6ecf2a99994244724e823f2f7dc9634f18
Manni <jm_jakobmann@web.de>
2023-02-17 18:18:49 +0100

    misc

--- resources/js/Pages/Eintragen/Index_backup.vue
+++ resources/js/Pages/Eintragen/Index_backup.vue
@@ -5,7 +5,7 @@ import InputError from "@/Components/InputError.vue";
 import { useForm, Head, Link } from "@inertiajs/inertia-vue3";
 import { computed, ref, Transition } from "vue";
 import Card from "@/Components/Card.vue";
-import moment from "moment";
+import dayjs from "dayjs";
 
 const submit_disabled = ref(true); // TODO
 const submitted = ref(false);
@@ -253,8 +253,8 @@ const zeitraum = computed({
                         :value="zeitraum"
                         class="form-select-option"
                       >
-                        {{ moment(zeitraum.von).format("DD.MM.YYYY") }} -
-                        {{ moment(zeitraum.bis).format("DD.MM.YYYY") }}
+                        {{ dayjs(zeitraum.von).format("DD.MM.YYYY") }} -
+                        {{ dayjs(zeitraum.bis).format("DD.MM.YYYY") }}
                         <!-- {{
                           zeitraum.von.split("-").reverse().join(".") +
                           " - " +
