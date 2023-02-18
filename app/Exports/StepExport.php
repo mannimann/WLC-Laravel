2f0cff9b3f73917f2e002c1a51ec04aa62e4a5a8
Manni <jm_jakobmann@web.de>
2023-02-18 13:26:32 +0100

    export to excel

--- app/Exports/StepExport.php
+++ app/Exports/StepExport.php
@@ -18,38 +18,4 @@ class StepExport implements WithMultipleSheets
 
     return $sheets;
   }
-
-  //   protected $sheets;
-
-  //   /**
-  //    * @return array
-  //    */
-  //   public function sheets(): array
-  //   {
-  //     $sheets = [];
-
-  //     // for ($month = 1; $month <= 12; $month++) {
-  //     //   $sheets[] = new Step($this->year, $month);
-  //     // }
-  //     $sheets[0] = Step::all();
-  //     return $sheets;
-  //     // return Step::all();
-  //   }
-
-  //   /**
-  //    * @return string
-  //    */
-  //   public function title(): string
-  //   {
-  //     return "Month ";
-  //   }
-
-  //   /**
-  //    * @return \Illuminate\Support\Collection
-  //    */
-  //   public function collection()
-  //   {
-  //     $file = StepExport::sheets();
-  //     return $file;
-  //   }
 }
